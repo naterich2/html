941696f0d025a6a868d24c9622c47bb420610e15
nate.rich2 <nate.rich2@gmail.com>
2017-07-25 19:55:59 -0500

    Made script for printing temperature to console

---
+++ server/pi-room-temp.c
@@ -0,0 +1,58 @@
+#include <wiringpi.h>
+#include <stdio.h>
+#include <stlib.h>
+#include <stdint.h>
+
+#define DHTpin = 21;
+
+int main(){
+  wiringPiSetupGpio();
+  while(1){
+    read_dat();
+    delay(1000);
+  }
+}
+
+
+int dat[5]={0,0,0,0,0};
+void read_dat(){
+  int counter =0;
+  int i=0, j=0;
+  float f;
+  uint8_t laststate = HIGH;
+  dat[0]=dat[1]=dat[2]=dat[3]=dat[4]=0;
+
+  pinMode(DHTpin, OUTPUT);
+  digitalWrite(DHTpin, LOW); //Send start signal 20ms Low
+  delay(20);
+
+  digitalWrite(DHTpin, HIGH); //Pull up for 20-40ms
+  delay(40);
+
+  pinMode(DHTpin, INPUT);   //pin will be input now
+  for(i=0; i<85;i++){
+    counter=0;                              //Cycle through each
+    while(digitalRead(DHTpin)==laststate){  //change in state
+      counter++;                            //description                           Count after loop
+      delay(1);                             //Time till sensor changes --             1
+      if(counter ==255)                     //Sensor pulls low                        2
+        break;                              //Sensor pulls handle_signals             3
+    }                                       //Sensor starts transmission with Low     4
+    laststate = digitalRead(DHTpin);        //First bit starts at count 4
+    if(counter == 255)                      //Second bit starts 2 after that (low then high)
+      break;                                //so every even cycle starting at 4 is high data bit
+
+    if((i>=4) && (i %2 ==0)){
+      dat[j/8]=dat[j/8]<< 1                 //Shift data at j/8 byte left by one to accomodate next bit
+      if(counter > 16)                      //If its a high bit
+        dat[j/8]=dat[j/8] | 1;              //Or with 1 to append bit
+        j++;                                //increment bit
+    }
+  }
+  if(j>=40 && dat[4]=((dat[0]+dat[1]+dat[2]+dat[3]) & 0xFF)){
+    //good data 40 bits and checkbyte
+    f = dat[2]*9./5.+32;
+    printf("humidity= %d.%d %% Temperature = %d.%d C\n",
+            dat[0],dat[1],dat[2],dat[3]);
+  }
+}
