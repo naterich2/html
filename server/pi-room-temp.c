0761a3d0f2a0fea1954e31cb51746676f1725a6a
naterich2 <nate.rich2@gmail.com>
2017-07-31 19:28:45 -0500

    Adjusted driver

--- server/pi-room-temp.c
+++ server/pi-room-temp.c
@@ -3,24 +3,97 @@
 #include <stdlib.h>
 #include <stdint.h>
 
-#define PIN 21
+#define PIN 7
+
+#define DHT_MAXCOUNT 32000
+#define DHT_PULSES 41
+
+
+
+
+
+void readd(uint8_t pin){
+  pinMode(pin, OUTPUT);
+  digitalWrite(pin, LOW);  //Write low for 2 milliseconds
+  delay(2);                //pullup resistor writes high until
+  pinMode(pin, INPUT);     //sensor responds
+
+  for(int i=0; i<2000; i++){
+    if(digitalRead(pin)==LOW) break;
+  }
+  int i;
+  int d_buff[41];
+  for(int j=0; j < DHT_MAXCOUNT; j++){
+    for(int i=1; i<2000; i++){
+      if(digitalRead(pin) == HIGH) break;
+    }
+    for(i=1; i<2000; i++){
+      if(digitalRead(pin) == LOW) break;
+    }
+    d_buff[j] = i;
+  }
+
+  uint8_t byte1 = getByte(1, d_buff);
+  uint8_t byte2 = getByte(2, d_buff);
+  uint8_t byte3 = getByte(3, d_buff);
+  uint8_t byte4 = getByte(4, d_buff);
+  uint8_t byte5 = getByte(5, d_buff);
+  if((byte1+byte2+byte3+byte4) & 0xFF == byte5){
+    float humidity = (float) (byte1 << 8 | byte2) / 10.0;
+    printf("Humidity= %f \n\r", humidity);
+    float temperature;
+    int neg = byte3 & 0x80;
+    byte3 = byte3 & 0x7F;
+    temperature = (float) (byte3 << 8 | byte4) / 10.0;
+    if (neg > 0)temperature = -temperature;
+    printf("Temperature= %f \n\r", temperature);
+    return;
+  } else {
+    printf("data not good\n");
+  }
+}
+
+uint8_t getByte(int b, int buff[]){
+  int i;
+  uint8_t result = 0;
+  b = (b-1)*8+1;
+  for(i=b; i<=(b+7_;i++){
+    result = result <<1;
+    result = result | buf[i];
+  }
+  return result;
+}
 
 
 
 int dat[5]={0,0,0,0,0};
 void read_dat(){
-  int counter =0;
+   counter =0;
   int i=0, j=0;
   float f;
   uint8_t laststate = HIGH;
   dat[0]=dat[1]=dat[2]=dat[3]=dat[4]=0;
 
   pinMode(PIN, OUTPUT);
+  digitalWrite(PIN, HIGH);
+  delay(250);
+
   digitalWrite(PIN, LOW); //Send start signal 20ms Low
   delay(20);
 
-  digitalWrite(PIN, HIGH); //Pull up for 20-40ms
-  delay(40);
+
+  pinMode(PIN, INPUT);
+  for(volatile int i=0;i<50; i++){}//Delay before reading
+
+  //wait for DHT to pull low
+  uint32_t count = 0;
+  for(int x=0;x<200;x++){}
+  while(digitalRead(PIN)==HIGH){
+    if(++count >= DHT_MAXCOUNT){
+
+    }
+  }
+
 
   pinMode(PIN, INPUT);   //pin will be input now
   for(i=0; i<85;i++){
@@ -53,7 +126,7 @@ void read_dat(){
 int main(void){
   wiringPiSetupGpio();
   while(1){
-    read_dat();
+    readd();
     delay(1000);
   }
 }
